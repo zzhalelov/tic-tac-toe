1f7ce3e65ef349d9916b3faedb32287ad367e6c1
Zhastilek Zhalelov <zzhalelov@gmail.com>
2023-10-19 22:18:56 +0600

    get the player names; create Tic Tac Toe board; fill the boards with dashes

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        //Get the player's names
+        System.out.println("Player 1, what's your name? ");
+        String p1 = in.nextLine();
+        System.out.println("Player 2, what's your name? ");
+        String p2 = in.nextLine();
+        //3x3 Tic Tac Toe board
+        //- empty space
+        //x player 1
+        //o player 2
+        char[][] board = new char[3][3];
+        //Fill the boards with dashes
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '-';
+            }
+        }
+    }
+}
